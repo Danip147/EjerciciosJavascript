f3c406b359741407b85a0e400107468f3130e5ea
Dani <danielporras147@outlook.es>
2023-01-30 17:49:46 +0100

    Ejercicio 02

---
+++ Ejercicio02/ejercicio02.js
@@ -0,0 +1,12 @@
+/**
+ * 2. Crea una función que se cargue al inicio 
+ * y que muestre la suma de dos números.
+ */
+
+function suma(){
+    let n1 = 9;
+    let n2 = 10;
+
+    document.write(n1 + n2);
+}
+
